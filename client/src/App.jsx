c3f412e377984dc3c5e5a28ec04bf881593f0e6a
VincentSlk <slkvct@gmail.com>
2023-11-25 18:59:37 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Body from './component/Body'
+import Footer from './component/Footer'
+
+function App() {
+
+  return (
+    <div className='h-[100vh]'>
+      
+    
+      <Body/>
+      
+      <Footer/>
+    
+
+    </div>
+  )
+}
+
+export default App
