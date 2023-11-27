83eb8b059786773373853fb5410d66a5a4cc58eb
VincentSlk <slkvct@gmail.com>
2023-11-27 15:31:56 +0100

    Erreur404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import Menu from "./component/Menu/Menu.jsx"
 import Salon from "./component/Salon/Jeu1.jsx"
 import {Routes , Route} from "react-router-dom"
 
+import Erreur from "./component/Erreur.jsx"
 
 function App() {
   return (
     <>
     <Routes>
     <Route path="/" element={<Menu/>}/>
-    <Route path="/Salon/:JeuX" element={<Salon/>} />
+    <Route path="/Salon/:Numero" element={<Salon/>} />
+    <Route path="/Salon/*" element={<Erreur/>} />
     </Routes>
     </>
   );
---
+++ client/src/component/Erreur.jsx
@@ -0,0 +1,9 @@
+export default function Erreur() {
+  return (
+
+    <div className="flex items-center justify-center h-screen"> 
+        <div className='Erreur text-center text-black uppercase font-bold p-40 rounded-md'>Erreur</div>
+    </div>
+
+  )
+}
